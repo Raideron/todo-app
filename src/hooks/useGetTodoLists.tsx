217e399ed21582b7dd4cbef0919bc63e6d599e99
Alex Poley <alex.poley14@gmail.com>
2024-04-06 13:45:08 +0200

    Fix TS error

--- src/hooks/useGetTodoLists.tsx
+++ src/hooks/useGetTodoLists.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
-import PocketBase from 'pocketbase';
 
 import { GET_TODO_LISTS } from '@/api/api-keys';
-import { POCKET_BASE_URL } from '@/constants/pocketbase';
 import { pb } from '@/pocketbase';
 import { TodoList, TodoListSchema } from '@/types/todo-list';
 
